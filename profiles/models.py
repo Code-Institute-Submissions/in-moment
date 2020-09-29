d13026377d5cf78a41b330e6d0e15cdd0e01fe08
Yiaoma <a.o.kalugin@gmail.com>
2020-09-29 20:30:57 +0100

    Updated allauth ui, added links for login, register and account views.

--- profiles/models.py
+++ profiles/models.py
@@ -23,4 +23,4 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
     # If user exists just save it.
-    insatnce.userprofile.save()
+    instance.userprofile.save()
