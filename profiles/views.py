1a287071bc83622c3b1ce6d46c22aa47f32c4ba2
Yiaoma <a.o.kalugin@gmail.com>
2020-09-29 08:59:12 +0100

    Created profiles app.

---
+++ profiles/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def profile(request):
+    template = "profile/profile.html"
+    context = {}
+
+    return render(request, template, context)
