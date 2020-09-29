1ea972ad0de2349c9b49a80015c2cd74a3af6558
Yiaoma <a.o.kalugin@gmail.com>
2020-09-29 21:34:19 +0100

    Added profile update form and autocomplete on orders.

--- profiles/views.py
+++ profiles/views.py
@@ -23,3 +23,15 @@ def profile(request):
     }
 
     return render(request, template, context)
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    template = "checkout/checkout_success.html"
+
+    context = {
+        "order": order,
+        "from_profile": True,
+    }
+
+    return render(request, template, context)
