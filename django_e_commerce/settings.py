ed693eb31fa7f59ef0143bc4b5ae58a1057e0e25
Yiaoma <a.o.kalugin@gmail.com>
2020-09-30 23:23:04 +0100

    Moved secret key to enviroment variables

--- django_e_commerce/settings.py
+++ django_e_commerce/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2jhltxn@8h-4_&$q%27crv1#b)dwm#crn-wsb=6@g!2k)#9g9n'
+SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "DEVELOPMENT" in os.environ
 
 ALLOWED_HOSTS = ["in-moment.herokuapp.com", "localhost"]
 
