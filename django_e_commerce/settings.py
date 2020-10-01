6c2c590ffb1ac8bd235334875023adc416604406
Yiaoma <a.o.kalugin@gmail.com>
2020-10-01 07:48:44 +0100

    Added databases to settings.

--- django_e_commerce/settings.py
+++ django_e_commerce/settings.py
@@ -110,15 +110,15 @@ WSGI_APPLICATION = 'django_e_commerce.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 if "DATABASE_URL" in os.environ:
-    DATABASE = {
+    DATABASES = {
         "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
 else:
     DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': BASE_DIR / 'db.sqlite3',
-     }
+        'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
+        }
     }
 
 
